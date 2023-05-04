5b3171d788f66073337ab04a37c43fbd12041faf
Vincentius Roger Kuswara <vincentiusrkuswara@gmail.com>
2023-05-05 03:44:56 +0700

    remove no cors

--- src/pages/chat/ChatView.tsx
+++ src/pages/chat/ChatView.tsx
@@ -188,7 +188,6 @@ function ChatView({ chatMessages, setChatMessages, documentList }: ChatProps) {
             ...newMessage,
             file: docName,
           }),
-          mode: 'no-cors',
         }
       );
 
@@ -244,7 +243,6 @@ function ChatView({ chatMessages, setChatMessages, documentList }: ChatProps) {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify(newMessage),
-          mode: 'no-cors',
         }
       );
       console.log('RESPONSE', response);
